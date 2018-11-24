00abcb10033c57f6c5f44a08b8b713ffda890d1a
JesseGabrielGames <44761808+JesseGabrielGames@users.noreply.github.com>
2018-11-24 18:10:06 -0500

    update2

---
+++ Assets/Scripts/DestroyPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayer : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        { // check if it's the player, if you want
+            Destroy(other.gameObject);
+    }
+    } }
