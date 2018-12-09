f42eb0bf658d4604511aceb4641c00b2e6e71f7b
JesseGabrielGames <44761808+JesseGabrielGames@users.noreply.github.com>
2018-12-09 16:31:47 -0500

    added second camera

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Camera MainCamera;
+    public Camera FinalCamera;
+
+    public void ShowMainCamera()
+    {
+        MainCamera.enabled = true;
+        FinalCamera.enabled = false;
+    
+    }
+    public void ShowFinalCamera()
+    {
+        MainCamera.enabled = false;
+        FinalCamera.enabled = true;
+    }
+	
+}
